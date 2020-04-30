41e6a139d2a7fe8b076f3231022764ad602a1014
Yung Sheng Cheng <yungsheng.cheng@mail.utoronto.ca>
2020-04-30 19:57:13 -0400

    Modified gitignore and added basic search function that scrapes info from youtube

---
+++ ytscrape.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+from typing import List, Tuple, Dict
+
+import requests
+import pprint
+import re
+
+
+class YTScrape:
+    _url: str
+
+    def __init__(self, keyword: str):
+        self._url = 'https://www.youtube.com/results?search_query=' + keyword
+
+    def scrape(self) -> List:
+        result = []
+
+        soup = BeautifulSoup(requests.get(self._url).text, 'html.parser')
+        data = soup.find_all(attrs={'class': 'yt-uix-tile-link'})
+
+        if not data:
+            return []
+
+        for i in range(len(data)):
+            href = data[i]['href']
+            title = data[i]['title']
+
+            if 'channel' not in href and '&list=' not in href:
+                link = 'https://www.youtube.com' + href
+
+                duration_raw = data[i].find_next_sibling().text
+
+                duration1 = re.findall('-(.*?)\.', duration_raw)
+                duration2 = re.findall('(.*?)\.', duration_raw)
+
+                if duration1 and not duration2:
+                    duration = duration1[0].strip()
+                elif not duration1 and duration2:
+                    duration = duration2[0].strip()
+                else:
+                    duration = duration1[0].strip()
+
+                result.append((title, link, duration))
+
+        return result
+
+if __name__ == '__main__':
+    test = YTScrape(input('Enter a search keyword: '))
+    pprint.pprint(test.scrape())
